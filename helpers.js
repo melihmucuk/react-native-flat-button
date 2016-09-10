d0f0258618abd94057496f8a4973855cb7b9b154
Charles Mangwa <charlesmangwa@gmail.com>
2016-09-11 01:22:08 +0200

    chore(*): implement getDefaultStyle method
    - add external helper to get default style
    - minor refactoring

--- helpers.js
+++ helpers.js
@@ -1,11 +1,48 @@
 import { buttonStyles } from './styles'
 
 let defaultStyle = {}
+
 let pressInStyle = {
   borderColor: 'transparent',
   borderBottomWidth: 0,
   borderLeftWidth: 0,
   borderRightWidth: 0
+}
+
+export const getDefaultStyle = (type, props) => {
+  switch (type) {
+    case "custom":
+      defaultStyle = {
+        borderRadius: props.borderRadius,
+        borderBottomWidth: props.shadowHeight,
+        backgroundColor: props.backgroundColor,
+        borderColor: props.borderColor,
+        borderLeftWidth: props.borderLeftWidth,
+        borderRightWidth: props.borderRightWidth
+      }
+      return defaultStyle
+    case "primary":
+      defaultStyle = buttonStyles.primary
+      return defaultStyle
+    case "neutral":
+      defaultStyle = buttonStyles.neutral
+      return defaultStyle
+    case "warn":
+      defaultStyle = buttonStyles.warn
+      return defaultStyle
+    case "negative":
+      defaultStyle = buttonStyles.negative
+      return defaultStyle
+    case "positive":
+      defaultStyle = buttonStyles.positive
+      return defaultStyle
+    case "info":
+      defaultStyle = buttonStyles.info
+      return defaultStyle
+    default:
+      defaultStyle = buttonStyles.primary
+      return defaultStyle
+  }
 }
 
 export const getPressInStyle = (type, backgroundColor, borderRadius) => {
--- index.js
+++ index.js
@@ -5,22 +5,22 @@ import {
   TouchableOpacity
 } from 'react-native'
 import { buttonStyles, styles } from './styles'
-import { getPressInStyle } from './helpers'
+import { getDefaultStyle, getPressInStyle } from './helpers'
 
 class Button extends Component {
-    static propTypes = {
-        ...TouchableOpacity.propTypes,
-        type: React.PropTypes.string.isRequired,
-        backgroundColor: React.PropTypes.string,
-        borderColor: React.PropTypes.string,
-        borderRadius: React.PropTypes.number,
-        shadowHeight: React.PropTypes.number,
-        borderLeftWidth: React.PropTypes.number,
-        borderRightWidth: React.PropTypes.number,
-        activeOpacity: React.PropTypes.number,
-        containerStyle: View.propTypes.style,
-        contentStyle: Text.propTypes.style
-    }
+  static propTypes = {
+      ...TouchableOpacity.propTypes,
+      type: React.PropTypes.string.isRequired,
+      backgroundColor: React.PropTypes.string,
+      borderColor: React.PropTypes.string,
+      borderRadius: React.PropTypes.number,
+      shadowHeight: React.PropTypes.number,
+      borderLeftWidth: React.PropTypes.number,
+      borderRightWidth: React.PropTypes.number,
+      activeOpacity: React.PropTypes.number,
+      containerStyle: View.propTypes.style,
+      contentStyle: Text.propTypes.style
+  }
 
      static defaultProps = {
          borderRadius: 8,
@@ -48,66 +48,23 @@ class Button extends Component {
         this._pressIn = this._pressIn.bind(this)
         this._pressOut = this._pressOut.bind(this)
 
-        let defaultStyle = {}
-
-        switch (type) {
-          case "custom":
-            defaultStyle = {
-              borderRadius: borderRadius,
-              borderBottomWidth: shadowHeight,
-              backgroundColor: backgroundColor,
-              borderColor: borderColor,
-              borderLeftWidth: borderLeftWidth,
-              borderRightWidth: borderRightWidth
-            }
-            break
-
-          case "primary":
-            defaultStyle = buttonStyles.primary
-            break
-
-          case "neutral":
-            defaultStyle = buttonStyles.neutral
-          break
-
-          case "warn":
-            defaultStyle = buttonStyles.warn
-          break
-
-          case "negative":
-            defaultStyle = buttonStyles.negative
-          break
-
-          case "positive":
-            defaultStyle = buttonStyles.positive
-          break
-
-          case "info":
-            defaultStyle = buttonStyles.info
-          break
-
-          default:
-            defaultStyle = buttonStyles.primary
-        }
-
         this.state = {
-            defaultStyle,
-            pressInStyle: getPressInStyle(type, backgroundColor, borderRadius),
-            style: defaultStyle,
-            isBorderPresent: true,
+          style: getDefaultStyle(type, this.props),
+          pressInStyle: getPressInStyle(type, backgroundColor, borderRadius),
+          isBorderPresent: true,
         }
     }
 
     _pressIn(){
-        this.setState({
-            isBorderPresent: false,
-        })
+      this.setState({
+          isBorderPresent: false,
+      })
     }
 
     _pressOut(){
-        this.setState({
-            isBorderPresent: true,
-        })
+      this.setState({
+          isBorderPresent: true,
+      })
     }
 
 
